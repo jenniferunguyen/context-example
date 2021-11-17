838ed89220ddadc8d083d678873a639738529d93
jenniferunguyen <jenguyen@chapman.edu>
2021-11-16 18:32:07 -0800

    ItemContext

--- context/ItemContext.js
+++ context/ItemContext.js
@@ -1,1 +1,17 @@
 /* TODO: Create the ItemContext and useItems() function */
+
+import { createContext, useContext } from 'react';
+
+/* Creates the actual Context objects */
+export const ItemContext = createContext({
+  item: {}, // data object stored
+  setItem: () => {} // updater function so child components can modify
+});
+
+/* 
+* Utility function for whenever you want to 
+* consume the context data or setter function
+*/
+export function useItems() {
+  return useContext(ItemContext);
+}
